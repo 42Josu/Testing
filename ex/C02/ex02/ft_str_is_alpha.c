3888d07be3036b5e52cfc4ad5cc49d1c0af0a71e
Josu Enbeita <jenbeita@c1r5s4.42urduliz.com>
2024-06-17 11:04:13 +0200

    gf

--- ex/C02/ex02/ft_str_is_alpha.c
+++ ex/C02/ex02/ft_str_is_alpha.c
@@ -17,9 +17,9 @@ int	ft_str_is_alpha(char *str)
 	i = 0;
 	while (str[i] && str[i] != '\0')
 	{
-		if (str[i] < 'A' && str[i] > 'Z')
-			if (str[i] < 'a' && str[i] > 'z')
-				return (0);
+		if (str[i] < 'A' && str[i] > 'Z' 
+			&& str[i] < 'a' && str[i] > 'z')
+			return (0);
 		i++;
 	}
 	return (1);
