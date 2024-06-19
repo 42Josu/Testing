ee30b39a5ea6c37734c031f9e765c758f22ed7e3
Josu Enbeita <jenbeita@c1r8s3.42urduliz.com>
2024-06-19 15:58:13 +0200

    egr

--- C05/ex04/ft_fibonacci.c
+++ C05/ex04/ft_fibonacci.c
@@ -5,14 +5,15 @@ int ft_fibonacci(int index)
 	i[0] = 1;
 	i[1] = 0;
 	i[2] = 0;
+	i[3] = 0;
 	if (index < 0)
 		return (-1);
-	while (i[3] <= index)
+	while (i[3] < index)
 	{
 		i[2] = i[1];
 		i[1] = i[0];
 		i[0] = (i[0] + i[2]);
 		i[3]++;
 	}
-	return (0);
+	return (i[3]);
 }
