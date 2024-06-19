3be62fa408b8a2280ff9ab5e99eb7e5c3a0f3999
Eiren <eiren@eiren.studio>
2024-06-19 11:55:23 +0000

    fewrd

---
+++ C05/ex03/ft_recursive_power.c
@@ -0,0 +1,14 @@
+int lerele(int nb, int power, int val)
+{
+	if (power == 0)
+		return (1);
+	else if (power == 1)
+		return (val);
+	else if (power >= 1)
+		return lerele(nb, power - 1, (val * nb));
+	return (0);
+}
+int ft_recursive_power(int nb, int power)
+{
+	return (lerele(nb, power, nb));
+}
