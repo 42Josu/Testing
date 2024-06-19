1560cdd0950d72a982b54996d43b2f3ea20aaf8a
Josu Enbeita <jenbeita@c1r8s3.42urduliz.com>
2024-06-19 16:49:54 +0200

    fg

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -19,7 +19,7 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	i = 0;
 	if (n < 1)
 		return (0);
-	while (s1[i] == s2[i] && n - 1 > i)
+	while (s1[i] == s2[i] && n > i)
 		i++;
 	if (s1[i] && s2[i])
 		if ((s1[i] - s2[i]) > 0)
