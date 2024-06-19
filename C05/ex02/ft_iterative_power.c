7c792807c2e31102fd3c7c6014ca4050d7139189
Josu Enbeita <jenbeita@c1r8s3.42urduliz.com>
2024-06-19 15:35:56 +0200

    egr

--- C05/ex02/ft_iterative_power.c
+++ C05/ex02/ft_iterative_power.c
@@ -5,10 +5,12 @@ int ft_iterative_power(int nb, int power)
 
 	i = 1;
 	k = 0;
-	if (power == 0)
+	if (power == 0 && nb == 0)
 		return (1);
 	else if (power == 1)
 		return (nb);
+	else if (power < 0)
+		return (0);
 	else if (power >= 1)
 		while (i <= power)
 		{
