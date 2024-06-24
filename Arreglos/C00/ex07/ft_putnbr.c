dd78861549e10f46a698a78af149209b624a8623
Eiren <eiren@eiren.studio>
2024-06-24 07:41:32 +0000

    Arreglos C00 ex 6 y 7

---
+++ Arreglos/C00/ex07/ft_putnbr.c
@@ -0,0 +1,42 @@
+#include <unistd.h>
+
+int checkminus(int nb)
+{
+	if(nb < 0)
+	{
+		write(1, "-", 1);
+		if(nb == -2147483648)
+		{
+			write(1, "2", 1);
+			nb += 2000000000;
+		}
+		nb *= -1;
+	}
+	return (nb);
+}
+
+void ft_putnbr(int nb)
+{
+	char	k[256];
+	int		i;
+	
+	i = 0;
+	nb = checkminus(nb);
+	while(nb > 9)
+	{
+		k[i] = (nb % 10) + '0';
+		nb /= 10;
+		i++;
+	}
+	k[i] = nb + '0';
+	while(i >= 0)
+	{
+		write(1, &k[i], 1);
+		i--;
+	}
+}/*
+int main()
+{
+	ft_putnbr(-2147483648);
+	return (1);
+}*/
---
+++ Arreglos/C00/ex08/ft_print_combn.c
@@ -0,0 +1,10 @@
+void ft_print_combn(int n)
+{
+	int	i;
+	
+	i = 0;
+	while(i < n)
+	{
+		
+	}
+}
