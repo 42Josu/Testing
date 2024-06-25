e48929be9516344fed8e7024333945375b5db595
Josu Enbeita <jenbeita@c1r4s1.42urduliz.com>
2024-06-25 13:38:01 +0200

    Commit inicial para el ejercicio 3

---
+++ repo_1719315480/ex01/ft_strncmp.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jenbeita <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/16 13:34:37 by jenbeita          #+#    #+#             */
+/*   Updated: 2024/06/16 13:34:40 by jenbeita         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s1[i] && s1[i] == s2[i] && n > i)
+		i++;
+	if (n > i)
+	{
+		if (s1[i] && s2[i])
+			return ((int)(s1[i] - s2[i]));
+		else if (s1[i])
+			return ((int)(s1[i]));
+		else if (s2[i])
+			return ((int)(0 - s2[i]));
+	}
+	return (0);
+}
+/*
+int	main(void)
+{
+	char *s1;
+	char *s2;
+	unsigned int n;
+	
+	s1 = "hola";
+	s2 = "holo";
+	n = 4;
+	printf("%i", ft_strncmp(s1, s2, n));
+	return (1);
+}*/
